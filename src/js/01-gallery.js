7ac5f992147b29da24fc718a1b4782281127ef10
Katia Bahrin <katia.b1101@gmail.com>
2023-10-22 19:52:49 +0200

    fixed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
 
+const markup = createMarkup(galleryItems);
+galleryEl.insertAdjacentHTML('beforeend', markup);
+
+function createMarkup(array) {
+    return array.map(({ preview, original, description }) => {
+        return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+    }).join('');
+};
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 console.log(galleryItems);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
 
+import throttle from 'lodash.throttle';
+
+const formEl = document.querySelector('.feedback-form');
+const FEEDBACK_KEY = "feedback-form-state";
+let parsetData = {};
+
+formEl.addEventListener('input', throttle(onInputHandler, 500));
+formEl.addEventListener('submit', onFormSubmitHandler);
+updateForm();
+
+function onInputHandler(event) {
+    const emailValue = event.currentTarget.email.value;
+    const messageValue = event.currentTarget.message.value;
+
+    parsetData = {
+       email: emailValue,
+       message: messageValue,
+    };
+
+    localStorage.setItem(FEEDBACK_KEY, JSON.stringify(parsetData))
+}
+
+function onFormSubmitHandler(event) {
+    event.preventDefault();
+    const { email, message } = event.currentTarget.elements;
+
+    if (email.value === '' || message.value === '') {
+        return alert("All fields must be filed");
+    }
+
+    console.log({ email: email.value, message: message.value });
+
+    event.currentTarget.reset();
+    localStorage.removeItem(FEEDBACK_KEY);
+    parsetData = {};
+}
+
+function updateForm() {
+    const inputData = localStorage.getItem(FEEDBACK_KEY);
+    if (inputData) {
+        parsetData = JSON.parse(inputData);
+       
+        let { email, message } = formEl.elements;
+    email.value = parsetData.email || '';
+    message.value = parsetData.message || '';
+    }
+}
